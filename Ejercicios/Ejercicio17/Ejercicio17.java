ed7394c82e6af9a0225e146306c88859cfb67b81
jotaeme890 <permanentjavi@gmail.com>
2023-01-26 08:52:08 +0100

    Ejercicio17

--- Ejercicios/Ejercicio17/Ejercicio17.java
+++ Ejercicios/Ejercicio17/Ejercicio17.java
@@ -1,3 +1,16 @@
+/**
+ * 17 Una empresa de venta por internet de productos electrónicos nos ha encargado
+ * implementar un carrito de la compra. Crea la clase Carrito. Al carrito se le
+ * pueden ir agregando elementos que se guardarán en una lista, por tanto,
+ * deberás crear la clase Elemento. Cada elemento del carrito deberá contener el
+ * nombre del producto, su precio y la cantidad (número de unidades de dicho
+ * producto). A continuación se muestra tanto el contenido del programa principal
+ * como la salida que debe mostrar el programa. Los métodos a implementar se
+ * pueden deducir del main.
+ * 
+ * @author jotaeme
+ */
+
 package Ejercicios.Ejercicio17;
 
 public class Ejercicio17 {
