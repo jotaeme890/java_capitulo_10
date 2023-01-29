b96ac9405c913de2d40ab99389fe7409a7613e13
jotaeme890 <permanentjavi@gmail.com>
2023-01-29 17:37:36 +0100

    Figura

---
+++ Ejercicios/Ejercicio23/Figura.java
@@ -0,0 +1,29 @@
+package Ejercicios.Ejercicio23;
+
+class Figura {
+    private String nombre;
+    private int valor;
+    private int cantidad;
+
+    public Figura(String nombre, int valor, int cantidad) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.cantidad = cantidad;
+    }
+
+    public boolean esCapturable() {
+        return this.cantidad > 0;
+    }
+    public void captura() {
+        this.cantidad--;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + valor + (valor == 1 ? " peón)" : " peones)");
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
