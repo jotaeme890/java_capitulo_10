e594c595ea58c4f14b57dac7ef1a96211ce310d1
jotaeme890 <permanentjavi@gmail.com>
2023-01-29 10:46:44 +0100

    Carrito

---
+++ Ejercicios/Ejercicio18/Carrito.java
@@ -0,0 +1,43 @@
+package Ejercicios.Ejercicio18;
+
+import java.util.ArrayList;
+
+public class Carrito {
+    ArrayList <Elemento> carrito = new ArrayList<>();
+
+    public void agrega (Elemento a){
+        boolean existe = false;
+
+        for (Elemento elemento : carrito) {
+            
+            if(elemento.getProducto().equals(a.getProducto())){
+                elemento.setCantidad(elemento.getCantidad() + a.getCantidad());
+                existe = true;
+            }
+        }
+        if(!existe){
+            carrito.add(a);
+        }
+    } 
+
+    public int numeroDeElementos() {
+        return carrito.size();
+    }
+
+    public double importeTotal() {
+        double total = 0;
+        for (Elemento e : carrito) {
+        total += e.getPrecio() * e.getCantidad();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        String pinta = "Contenido del carrito\n=====================\n";
+        for (Elemento e : carrito) {
+            pinta += e + "\n";
+        }
+        return pinta;
+    }
+}
