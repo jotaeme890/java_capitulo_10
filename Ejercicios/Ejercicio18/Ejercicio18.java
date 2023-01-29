1457c6c498f0c813eeb429a8389f6bd105103d3e
jotaeme890 <permanentjavi@gmail.com>
2023-01-29 10:55:13 +0100

    Ejercicio18

--- Ejercicios/Ejercicio18/Ejercicio18.java
+++ Ejercicios/Ejercicio18/Ejercicio18.java
@@ -1,3 +1,14 @@
+/**
+ * 18 Mejora el programa anterior (en otro proyecto diferente) de tal forma que al
+ * intentar agregar un elemento al carrito, se compruebe si ya existe el producto
+ * y, en tal caso, se incremente el número de unidades sin añadir un nuevo
+ * elemento. Observa que en el programa anterior, se repetía el producto “Tarjeta
+ * SD 64Gb” dos veces en el carrito. En esta nueva versión ya no sucede esto, si
+ * no que se incrementa el número de unidades del producto que se agrega.
+ * 
+ * @author jotaeme
+ */
+
 package Ejercicios.Ejercicio18;
 
 public class Ejercicio18 {
